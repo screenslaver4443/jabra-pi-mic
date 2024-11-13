d487380bccffb98c0a6368acbff0c04d994e825b
Nikolai Pesudovs <npesudovs@outlook.com>
2024-11-13 15:07:02 +1030

    added missing file in script

--- install.sh
+++ install.sh
@@ -99,7 +99,7 @@ exec > /tmp/usb_disconnect.log 2>&1
 echo "started @ $(date) "
 /bin/systemctl stop usb_arecord.service
 if [ -f $RECORDINGS_DIR/rec.wav ]; then
-	/bin/scp $RECORDINGS_DIR $SSHpath
+	/bin/scp $RECORDINGS_DIR $SSHpath/rec.wav
 	rm $RECORDINGS_DIR/rec.wav
 fi
 echo "finished"" > $INSTALL_DIR/usb_disconnect.sh
